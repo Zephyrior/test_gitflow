76de67df25565bef726cce1901d249a6c0b4de5a
Zephyrior <mark.temblique@gmail.com>
2025-05-02 12:22:20 +0200

    modifica Prenotazione class

--- src/main/java/it/epicode/test_gitflow/prenotazioni/Prenotazione.java
+++ src/main/java/it/epicode/test_gitflow/prenotazioni/Prenotazione.java
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 public class Prenotazione {
 
     private Long id;
+    private Long numeroPrenotazione;
     private Dipendente dipendente;
     private Viaggio viaggio;
 
